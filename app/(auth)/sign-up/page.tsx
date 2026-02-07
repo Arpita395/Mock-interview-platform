d0a861b8413b4ca8235655b8ef083d1376a8e935
Arpita395 <karpita395@gmail.com>
2026-02-07 19:26:55 +0530

    Add window icon SVG file to public directory

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,7 @@
+import AuthForm from "@/components/AuthForm"
+
 const Page= ()=> {
-    return (
-        <div>
-            Sign Up Page
-        </div>
-    )
+    return <AuthForm type="sign-up"/>
 }
 
 export default Page
